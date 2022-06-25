6597023660cecb85805408e08bb854ef7f61d451
Stella Ling <stellalph2@gmail.com>
2022-06-25 12:37:33 +0930

    Changes to add jobRoutes.js, and others

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const dashboardRoutes = require('../dashboardRoutes');
+const jobRoutes = require('./jobRoutes');
 
 router.use('/users', userRoutes);
 router.use('/signin', dashboardRoutes);
 router.use('/signup', dashboardRoutes);
+router.use('/jobs', jobRoutes);
 
 module.exports = router;
