834c8dd0c8abcaa5a86150fb9142b91c8aaddc6c
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-06-26 21:23:10 +0930

    Added DELETE route for deleting jobs

--- controllers/api/jobRoutes.js
+++ controllers/api/jobRoutes.js
@@ -22,4 +22,24 @@ router.post('/', async(req, res) => {
     }
 });
 
+router.delete('/:id', async(req, res) => {
+    try {
+        const jobData = await Job.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!jobData) {
+            res.status(404).json({ message: "No job with this id!" });
+            return;
+        }
+
+        res.status(200).json(jobData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router
